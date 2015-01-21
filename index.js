ea5b0cc4e0dcfb392884af4e8505a19e074143f7
Daniele Brugnara <daniele@brugnara.me>
2015-01-21 15:28:51 +0100

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+var settings = require('env-settings');
+var fs = require('fs');
+
+var Mongo = require('./lib/mongo');
+var Handlers = require('./lib/handlers');
+var handlers = new Handlers({
+  extraurbano: './gtfs/googletransitextraurbano/',
+  urbano: './gtfs/googletransiturbano/'
+});
+
+new Mongo(settings.mongo + 'tt_exu', handlers.extraurbano.bind(handlers));
+new Mongo(settings.mongo + 'tt_u', handlers.urbano.bind(handlers));
