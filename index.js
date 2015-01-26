6af755a6cb25e5c1077187d2160ca06350c19e68
Daniele Brugnara <daniele@brugnara.me>
2015-01-26 16:54:50 +0100

    insert in unico db, differenziazione per chiave

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var settings = require('env-settings');
-var fs = require('fs');
+var async = require('async');
 
 var Mongo = require('./lib/mongo');
 var Handlers = require('./lib/handlers');
@@ -8,5 +8,18 @@ var handlers = new Handlers({
   urbano: './gtfs/googletransiturbano/'
 });
 
-new Mongo(settings.mongo + 'tt_exu', handlers.extraurbano.bind(handlers));
-new Mongo(settings.mongo + 'tt_u', handlers.urbano.bind(handlers));
+// new Mongo(settings.mongo + 'tt_exu', handlers.extraurbano.bind(handlers));
+// new Mongo(settings.mongo + 'tt_u', handlers.urbano.bind(handlers));
+new Mongo(settings.mongo, function(err, db) {
+  async.parallel([
+    function(cb) {
+      handlers.extraurbano('EXU', db, cb);
+    },
+    function(cb) {
+      handlers.urbano('U', db, cb);
+    }
+  ], function(err, junk) {
+    err && console.error(err);
+    !err && console.log('DONE!');
+  })
+});
